fa7ca1bd7b94b915b2fb288c42f1b21a36fc86ed
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-12 16:03:17 -0400

    adds directions to asteroids

--- js/views/asteroids.js
+++ js/views/asteroids.js
@@ -1,6 +1,6 @@
 PersonalPages.Views.Asteroids = Backbone.View.extend({
 	// template: _.template("<div class='asteroids-body'><canvas class='asteroids-game' width='500' height='500'></canvas><section class='asteroids-contact'><p class='group'><a href='#'>Back to homepage</a></p><p class='group'>Created by<a href='#'>Greg Meyer</a></p><p class='group'><a href='https://github.com/gmmeyer/Asteroids'>Source</a></p></section></div>"),
-	template: _.template("<div class='asteroids-body'><canvas class='asteroids-game' width='500' height='500'></canvas></div>"),
+	template: _.template("<div class='asteroids-body'><p>use the W-A-S-D or the Arrow Keys to move, space to shoot.</p><canvas class='asteroids-game' width='500' height='500'></canvas></div>"),
 
 	initialize: function () {
 		console.log("lkjsdaflkjadflkdsjaflj");
