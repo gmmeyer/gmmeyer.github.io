a7351255ac20599dbb6ac6de4ee55e42ffe3fc90
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-02-23 18:36:09 -0500

    adds hubspot

--- js/views/index.js
+++ js/views/index.js
@@ -1,59 +1,59 @@
 PersonalPages.Views.Index = Backbone.View.extend({
 
-	initialize: function () {
-	},
+  initialize: function () {
+  },
 
-	template: _.template($(".homepage-template").html()),
+  template: _.template($(".homepage-template").html()),
 
-	render: function () {
-		var content = this.template();
-		this.$el.html(content);
-		return this;
-	},
+  render: function () {
+    var content = this.template();
+    this.$el.html(content);
+    return this;
+  },
 
-	events: {
-		"click .slider" : "openUp",
-		"click .close" : "closeDown",
-		"click .inactive" : "closeDown",
-		"click #skype" : "openUp",
-		"click #phone" : "openUp",
-		"click #email" : "openUp",
-		"click .asteroids" : "renderAsteroids"
-	},
+  events: {
+    "click .slider" : "openUp",
+    "click .close" : "closeDown",
+    "click .inactive" : "closeDown",
+    "click #skype" : "openUp",
+    "click #phone" : "openUp",
+    "click #email" : "openUp",
+    "click .asteroids" : "renderAsteroids"
+  },
 
-	openUp: function (event, directURL) {
-		if(!directURL){
-			event.preventDefault();
-		}
-	  if ( $( 
-		event.currentTarget.nextElementSibling ).is( ":hidden" ) ) {
-	    $( 
-		event.currentTarget.nextElementSibling ).slideDown( 10 );
-	  } else if (event.currentTarget.nextElementSibling.id = "isHidden") {
-	    $( 
-		event.currentTarget.nextElementSibling ).hide();
-	  }
-	},
+  openUp: function (event, directURL) {
+    if(!directURL){
+      event.preventDefault();
+    }
+    if ( $(
+    event.currentTarget.nextElementSibling ).is( ":hidden" ) ) {
+      $(
+    event.currentTarget.nextElementSibling ).slideDown( 10 );
+    } else if (event.currentTarget.nextElementSibling.id = "isHidden") {
+      $(
+    event.currentTarget.nextElementSibling ).hide();
+    }
+  },
 
-	closeDown: function () {
-		event.preventDefault();
-		console.log(event);
-	},
+  closeDown: function () {
+    event.preventDefault();
+    console.log(event);
+  },
 
-	renderAsteroids: function (directURL) {
-		event.preventDefault();
-		if(!directURL){
-			event.preventDefault();
-			PersonalPages.router.navigate("asteroids");
-		}
-		if(this._asteroidsRendered){
-			$('.asteroids-body').remove()
-			this._asteroidsRendered = false
-		} else {
-			var view = new PersonalPages.Views.Asteroids();
-			$('.asteroids-wrapper').append(view.render().$el)
-			this.my_game = view.startGame()
-	    this._asteroidsRendered = true
-		}
-	}
+  renderAsteroids: function (directURL) {
+    event.preventDefault();
+    if(!directURL){
+      event.preventDefault();
+      PersonalPages.router.navigate("asteroids");
+    }
+    if(this._asteroidsRendered){
+      $('.asteroids-body').remove()
+      this._asteroidsRendered = false
+    } else {
+      var view = new PersonalPages.Views.Asteroids();
+      $('.asteroids-wrapper').append(view.render().$el)
+      this.my_game = view.startGame()
+      this._asteroidsRendered = true
+    }
+  }
 })
